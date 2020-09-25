e4dc73cd0f9c9798ab8caa8419150d088b693d55
Jason Gonzales <jasongonzo25@gmail.com>
2020-09-25 13:31:55 -0700

    completed match gif

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var nextButton = document.querySelector("#next-btn");
 var infoName = document.querySelector("#info-name");
+var infoHair = document.querySelector("#info-hair");
+var infoEyes = document.querySelector("#info-eyes");
+var infoGender = document.querySelector("#info-gender");
 
 var gifButton = document.querySelector("#gif-btn");
 var gifContainer = document.querySelector("#gif");
 
+
 nextButton.addEventListener("click",getInfo);
-gifButton.addEventListener("click",getGif);
+// gifButton.addEventListener("click",getGif);
 
 function getInfo() {
-  var random = Math.floor((Math.random() * 10) + 1);
+  var random = Math.floor((Math.random() * 30) + 1);
   $.ajax({
     url: "https://swapi.dev/api/people/" + random,
     method: "GET",
@@ -16,7 +20,30 @@ function getInfo() {
     //   "Origin":null
     // },
     success: function (data) {
-      updateInfo(data.name, data.hair_color, data.eye_color);
+      clearImage();
+      updateInfo(data);
+      var character = data.name;
+      console.log(character);
+      //call the getGif()
+      //pass in the name property of the response
+      //use the value from name property and concat to your url in your request
+      //when requesting api
+      $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=tQvMLCK2J2FBihuD16peIhsL4BrkbhAU&q=starwars&limit=30&offset=0&rating=g&lang=en" + random,
+        method: "GET",
+        success: function (data) {
+          var image = document.createElement("img");
+          for (var i = 0; i < data.data.length; i++) {
+            image.src = data.data[i].images.original.url;
+          }
+
+          gifContainer.append(image);
+          image.className = "border-0";
+        },
+        error: function (error) {
+          console.error(error)
+        }
+      })
 
     },
     error: function (error) {
@@ -24,30 +51,34 @@ function getInfo() {
     }
   })
 }
-function updateInfo(name,hair,eye) {
-  infoName.innerText = name +": " + hair  + " hair" + ", "+  eye
-  + " eyes";
-  // if(name === "Luke Skywalker") {
-  //   infoName.innerText.classList.add("chewy");
-  // }
+function updateInfo(data) {
+  infoName.innerText = data.name;
+  infoHair.innerText = "hair color: " + data.hair_color;
+  infoEyes.innerText = "eye color: " + data.eye_color;
+  infoGender.innerText = "gender: " + data.gender;
 
 }
 
-
-function getGif() {
-  var random2 = Math.floor((Math.random() * 10) + 1);
-$.ajax({
-  url: "https://api.giphy.com/v1/gifs/search?api_key=tQvMLCK2J2FBihuD16peIhsL4BrkbhAU&q=starwars&limit=25&offset=0&rating=g&lang=en" + random2,
-  method: "GET",
-  success: function (data) {
-    console.log(data.data[0]);
-    var image = document.createElement("iframe");
-    image.src = data.data[1].embed_url;
-
-    gifContainer.append(image);
-  },
-  error: function (error) {
-    console.error(error)
-  }
-})
+function clearImage() {
+  gifContainer.textContent ="";
 }
+
+// function getGif() {
+//   var random2 = Math.floor((Math.random() * 10) + 1);
+// $.ajax({
+//   url: "https://api.giphy.com/v1/gifs/search?api_key=tQvMLCK2J2FBihuD16peIhsL4BrkbhAU&q=starwars&limit=30&offset=0&rating=g&lang=en" + random2,
+//   method: "GET",
+//   success: function (data) {
+//     var image = document.createElement("img");
+//     for(var i = 0; i < data.data.length; i++) {
+//     image.src = data.data[i].images.original.url;
+
+//     }
+//     gifContainer.append(image);
+//     image.className = "border-0";
+//   },
+//   error: function (error) {
+//     console.error(error)
+//   }
+// })
+// }
