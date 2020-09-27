4b79013d1b5e1ba7b5a543565738318c21e44adc
Jason Gonzales <jasongonzo25@gmail.com>
2020-09-27 00:33:54 -0700

    edit-buttons

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var infoHair = document.querySelector("#info-hair");
 var infoEyes = document.querySelector("#info-eyes");
 var infoGender = document.querySelector("#info-gender");
 
+
 var gifButton = document.querySelector("#gif-btn");
 var gifContainer = document.querySelector("#gif");
 
@@ -19,10 +20,11 @@ genPage.classList.remove("hidden");
 }
 
 nextButton.addEventListener("click",getInfo);
-// gifButton.addEventListener("click",getGif);
+gifButton.addEventListener("click",getGif);
 
-function getInfo() {
-  var random = Math.floor((Math.random() * 30) + 1);
+
+function getInfo(character) {
+  var random = Math.floor((Math.random() * 40) + 1);
   $.ajax({
     url: "https://swapi.dev/api/people/" + random,
     method: "GET",
@@ -30,31 +32,17 @@ function getInfo() {
     //   "Origin":null
     // },
     success: function (data) {
-      clearImage();
+
       updateInfo(data);
       var character = data.name;
-      console.log(character);
+
+      getGif(character);
       //call the getGif()
       //pass in the name property of the response
       //use the value from name property and concat to your url in your request
       //when requesting api
-      $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=tQvMLCK2J2FBihuD16peIhsL4BrkbhAU&q=starwars&limit=30&offset=0&rating=g&lang=en" + random,
-        method: "GET",
-        success: function (data) {
-          var image = document.createElement("img");
-          for (var i = 0; i < data.data.length; i++) {
-            image.src = data.data[i].images.original.url;
-          }
 
-          gifContainer.append(image);
-          image.className = "border-0";
-        },
-        error: function (error) {
-          console.error(error)
-        }
-      })
-
+      return character;
     },
     error: function (error) {
       console.error(error)
@@ -69,6 +57,27 @@ function updateInfo(data) {
 
 }
 
+function getGif(character) {
+$.ajax({
+  url: "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=tQvMLCK2J2FBihuD16peIhsL4BrkbhAU&q=starwars&limit=30&offset=0&rating=g&lang=en",
+  method: "GET",
+  success: function (data) {
+    clearImage();
+    var image = document.createElement("img");
+
+    console.log(character);
+    image.src = data.data[0].images.original.url;
+
+
+    gifContainer.append(image);
+    image.className = "border-0";
+  },
+  error: function (error) {
+    console.error(error)
+  }
+})
+
 function clearImage() {
   gifContainer.textContent ="";
+}
 }
